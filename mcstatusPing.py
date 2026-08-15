4fdc34877d168b4a7dc7b74840cbd68bf2155201
Eric Chen <echen0719@gmail.com>
2026-08-15 10:59:52 -0400

    Added async functions
    There is no way I am running this as sync functions or it will take too long. Added some error handling and made it less prone to problems as well.

--- mcstatusPing.py
+++ mcstatusPing.py
@@ -15,6 +15,7 @@ from slowapi.errors import RateLimitExceeded
 app = FastAPI(title="Minecraft Server Scanner")
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
+sephamore = asyncio.Semaphore(200)
 
 # added to make sure static website can access
 app.add_middleware(
@@ -39,6 +40,48 @@ def isInternal(ip):
     except ValueError:
         return True
 
+async def scanJava(address):
+    try:
+        server = await JavaServer.async_lookup(address)
+        status = await server.async_status()
+
+        return {
+            "online": True,
+            "motd": status.motd,
+            "version": status.version.name,
+            "protocol": status.version.protocol,
+            "players": {
+                "online": status.players.online,
+                "max": status.players.max
+            },
+            "playersList": status.players.sample,
+            "favicon": status.icon,
+        }
+
+    except Exception as e:
+        print("JavaServer error:", repr(e))
+        return None
+
+async def scanLegacy(address):
+    try:
+        server = await asyncio.to_thread(LegacyServer.lookup, address)
+        status = await server.async_status()
+
+        return {
+            "online": True,
+            "motd": status.motd,
+            "version": status.version.name,
+            "protocol": status.version.protocol,
+            "players": {
+                "online": status.players.online,
+                "max": status.players.max
+            },
+            "favicon": "",
+        }
+    except Exception as e:
+        print("LegacyServer error:", repr(e))
+        return None
+
 @app.get("/scan/{address}")
 @limiter.limit("5000/hour")
 async def scan(request: Request, address: str):
@@ -47,37 +90,19 @@ async def scan(request: Request, address: str):
     if not (0 < int(port) <= 65535) or isInternal(ip):
         raise HTTPException(status_code=400, detail="Invalid address")
 
-    try:
-        server = JavaServer.lookup(address)
-        status = await asyncio.to_thread(server.status)
+    async with sephamore:
+        try:
+            result = await asyncio.wait_for(scanJava(address), timeout=5)
+            if result is not None:
+                return result
+        except asyncio.TimeoutError:
+            pass
 
-        return {
-            "online": True,
-            "motd": status.motd,
-            "version": status.version.name,
-            "protocol": status.version.protocol,
-            "players_online": status.players.online,
-            "players_max": status.players.max,
-            "players_list": status.players.sample,
-            "favicon": status.icon,
-        }
+        try:
+            result = await asyncio.wait_for(scanLegacy(address), timeout=5)
+            if result is not None:
+                return result
+        except asyncio.TimeoutError:
+            pass
 
-    except Exception as e:
-        print("JavaServer error:", repr(e))
-
-    try:
-        server = LegacyServer.lookup(address)
-        status = await asyncio.to_thread(server.status)
-
-        return {
-            "online": True,
-            "motd": status.motd,
-            "version": status.version.name,
-            "protocol": status.version.protocol,
-            "players_online": status.players.online,
-            "players_max": status.players.max,
-            "favicon": "",
-        }
-    except Exception as e:
-        print("LegacyServer error:", repr(e))
-        return {"online": False}
+    return {"online": False}
